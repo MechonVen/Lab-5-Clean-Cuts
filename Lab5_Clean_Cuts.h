104bea957883ad915b956c87ee208d3a08b3e732
Jeremias <152121583+MechonVen@users.noreply.github.com>
2025-10-18 14:19:12 -0400

    Update Lab5_Clean_Cuts.h

--- Lab5_Clean_Cuts.h
+++ Lab5_Clean_Cuts.h
@@ -5,8 +5,39 @@
 #ifndef LAB5_CLEAN_CUTS_H
 #define LAB5_CLEAN_CUTS_H
 
-#endif //LAB5_CLEAN_CUTS_H
+class BSTNode {
+public:
+    int key;
+    BSTNode* left; //Pointer to left child node
+    BSTNode* right; //Pointer to right child node
+
+    BSTNode(int nodeKey, BSTNode* leftChild = nullptr, BSTNode* rightChild = nullptr);
+};
 
 class BinarySearchTree {
+private:
+    BSTNode* root;
 
+    void DeleteTree(BSTNode* subtreeRoot);
+
+public:
+    void BSTPrintInOrder(BSTNode* node);
+    void BSTPrintPreOrder(BSTNode* node);
+    void BSTPrintPostOrder(BSTNode* node);
+    //Function declaration for searching for a key
+    BSTNode* Search(int searchKey);
+
+    //Functions responsible for inserting new nodes and checking for duplicates
+    void InsertNode(BSTNode* newNode);
+    bool Contains(int key);
+    bool InsertKey(int key);
+
+    //Constructor and Destructor declarations
+    BinarySearchTree();
+    ~BinarySearchTree();
 };
+
+
+//This needs to be below every class and class function for the implementation
+//file to recognize
+#endif //LAB5_CLEAN_CUTS_H
