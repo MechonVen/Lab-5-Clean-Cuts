fb718f931adc593c68a62fb8756e4bb711e697a2
Jeremias <152121583+MechonVen@users.noreply.github.com>
2025-10-21 14:02:17 -0400

    Update main.cpp
    Finalized code and added comments

--- main.cpp
+++ main.cpp
@@ -2,14 +2,16 @@
 #include "Lab5_Clean_Cuts.h"
 
 int main() {
-    BinarySearchTree tree; //Create an object of the BST class
+    //Create an object of the BST class
+    BinarySearchTree tree;
 
     int n; //Variable for number of tree values
-    int num; //Variable for tree values
     std::cout << "Enter the number of values you want to enter into a BST:" << std::endl;
     std::cin >> n;
-    int array[n];
+    int array[n]; //Create an array of specified size to hold the elements that are to enter the BST
     std::cout << "Enter the elements of the tree:" << std::endl;
+
+    //Loop to take user input for BST elements/keys
     for (int i = 0; i < n; i++) {
         std::cin >> array[i];
     }
@@ -18,10 +20,11 @@ int main() {
     for (int i = 0; i < n; i++) {
         tree.InsertKey(array[i]);
     }
-    /* This idea does not work, ask Peter lol (probably need to use visitor functions instead)
+
+    //Print in, pre, and post-order traversals of the input BST
     tree.BSTPrintInOrder();
     tree.BSTPrintPreOrder();
     tree.BSTPrintPostOrder();
-    */
+
     return 0;
 }
