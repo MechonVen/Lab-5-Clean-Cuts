ebd71cbeff0264b2d3bb6f637b9fc06dc2529b72
Jeremias <152121583+MechonVen@users.noreply.github.com>
2025-10-21 14:00:59 -0400

    Update Lab5_Clean_Cuts.h
    Finalized code and added comments

--- Lab5_Clean_Cuts.h
+++ Lab5_Clean_Cuts.h
@@ -1,29 +1,35 @@
-//
-// Created by bebon on 10/16/2025.
-//
-
 #ifndef LAB5_CLEAN_CUTS_H
 #define LAB5_CLEAN_CUTS_H
 
+//Class for the nodes used in the BST implementation
 class BSTNode {
 public:
     int key;
     BSTNode* left; //Pointer to left child node
     BSTNode* right; //Pointer to right child node
 
+    //Default class constructor declaration to initialize variables
     BSTNode(int nodeKey, BSTNode* leftChild = nullptr, BSTNode* rightChild = nullptr);
 };
 
 class BinarySearchTree {
 private:
-    BSTNode* root;
+    BSTNode* root; //Declare root node to be used as start/root of BST
 
+    //Recursive function that goes through a subtree and deletes all nodes under it
     void DeleteTree(BSTNode* subtreeRoot);
 
+    //Recursive functions that travel through the BST according to the traversal type
+    void BSTInOrder(BSTNode* node);
+    void BSTPreOrder(BSTNode* node);
+    void BSTPostOrder(BSTNode* node);
+
 public:
-    void BSTPrintInOrder(BSTNode* node);
-    void BSTPrintPreOrder(BSTNode* node);
-    void BSTPrintPostOrder(BSTNode* node);
+    //Functions that print the results of the other traversal functions
+    void BSTPrintInOrder();
+    void BSTPrintPreOrder();
+    void BSTPrintPostOrder();
+
     //Function declaration for searching for a key
     BSTNode* Search(int searchKey);
 
@@ -37,7 +43,7 @@ public:
     ~BinarySearchTree();
 };
 
-
 //This needs to be below every class and class function for the implementation
 //file to recognize
 #endif //LAB5_CLEAN_CUTS_H
+
